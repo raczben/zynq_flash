fee177a5066fe60a85874df1320388b4839873e8
Benedek Racz <betontalpfa@gmail.com>
2020-02-13 21:10:04 +0100

    [CLN] Add comments, write README

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # zynq_flash
 QSPI flash support for Xilinx's Zynq devices
+
+## Motivation
+There are many problem around flash programming of the Zynq FPGAs. The task become harder if there is no DDR memory or the bootmode switch is unavailable, etc...
+This repo contains a simple flash util, to read, write, erase the QSPI flash attached to Zynq FPGAs.
+
+## How it work?
+You can build a *flash_writer.elf* executable from the given sources, which will handle the QSPI
+flash. This program runs the Zynq's ARM to give an interface for the XSCT/XSDB TCL console to access
+the QSPI flash. The *flash_writer.tcl* downloads this executable, and communicates with it to give
+handy interface for the user.
+
+## Try prebuilt on ZED board
+You can try the prebuilt executables on [ZEDBoard][1] (or any board with 7Z020 device.)
+
+  1. Open an [XSCT console][2].
+  2. Go to this repo's folder. `cd zynq_flash`
+  3. Source tcl sources: `source zed_bin/ps7_init.tcl` and `source flash_writer.tcl`
+  4. Connect and choose your ARM target: `connect` and `targets <num>`
+  5. Run initialization procedure: `init_flash_writer zed_bin/flash_writer.elf`
+  6. Run flash initialization: `flash_init`
+  7. Check with `get_flash_info` (optional)
+  8. Erase flash `flash_erase 0 0x500000` (This takes couple of secs)
+  9. Write the boot image `flash_write_file zed_bin/BOOT.bin` (Choose your own image, this example
+    boot-image will turn on all LEDs and turn of them based on the buttons.) (This takes a half minute)
+  10. Check with `flash_dump 0 64` and check the `aa995566` sync word at 0x20
+  11. Turn off/on the board and check the LEDs. If you have any problem feel free to contact me.
+
+[zed_bin_full_console.txt][3] is a full log of a flash programming.
+
+## Build flash_writer.elf from source
+The flash writer executable have to be re-built for different architectures. Here is a small
+ description how to do that:
+
+  1. Open Xilinx's XSDK.
+  2. Create a new Application project. Use your own hardware-platform / board-support-package.
+  Choose empty application. The [zed_bin/system.hdf][4] hardware definition for ZedBoard, so you can
+   use that hdf for ZedBoard flashing.
+  3. Copy the three sources: `main.c`, `flahs.c` `flash.h` into your application project's sources.
+  4. Refresh or rebuild your project and check the output in the Debug directory.
+
+
+[1]: http://zedboard.org/product/zedboard
+[2]: https://forums.xilinx.com/t5/Vivado-Debug-and-Power/Standalone-XSCT-console/m-p/907961/thread-id/12946#M12947
+[3]: zed_bin_full_console.txt
+[4]: zed_bin/system.hdf
--- flash_writer.tcl
+++ flash_writer.tcl
@@ -1,26 +1,89 @@
 
+####################################################################################################
+# Function IDs
+#
+# Global constants for communicating with the flash writer executables. Each *function id* can
+# "call" a C function.
+#
+# Do not change these values. These must be synchronous with the flash_writer executable.
+#
+####################################################################################################
 
-variable TCL_FUNCTION_ID_RETURN		 		0x0
-variable TCL_FUNCTION_ID_INIT		 		0x1
-variable TCL_FUNCTION_ID_PRINT_HELLO	 		0x10
-variable TCL_FUNCTION_ID_GET_VERSION	 		0x20
-variable TCL_FUNCTION_ID_FLASH_ERASE 		0x100
-variable TCL_FUNCTION_ID_GET_FLASH_INFO   	0x101
-variable TCL_FUNCTION_ID_FLASH_INIT	 		0x105
-variable TCL_FUNCTION_ID_FLASH_WRITE 		0x110
-variable TCL_FUNCTION_ID_FLASH_READ  		0x120
-variable TCL_FUNCTION_ID_FLASH_DUMP  		0x125
-variable TCL_FUNCTION_ID_GET_SLICE_BUFFER 	0x130
-variable TCL_FUNCTION_ID_GET_SLICE_BUFFER_SIZE 0x140
+variable TCL_FUNCTION_ID_RETURN                 0x0
+variable TCL_FUNCTION_ID_INIT                   0x1
+variable TCL_FUNCTION_ID_PRINT_HELLO            0x10
+variable TCL_FUNCTION_ID_GET_VERSION            0x20
+variable TCL_FUNCTION_ID_FLASH_ERASE            0x100
+variable TCL_FUNCTION_ID_GET_FLASH_INFO         0x101
+variable TCL_FUNCTION_ID_FLASH_INIT             0x105
+variable TCL_FUNCTION_ID_FLASH_WRITE            0x110
+variable TCL_FUNCTION_ID_FLASH_READ             0x120
+variable TCL_FUNCTION_ID_FLASH_DUMP             0x125
+variable TCL_FUNCTION_ID_GET_SLICE_BUFFER       0x130
+variable TCL_FUNCTION_ID_GET_SLICE_BUFFER_SIZE  0x140
 
-set flash_writer_elf zed_bin/flash_writer.elf
+# init_flash_writer saves here the path of flash_writer executable.
+if { ! [info exists flash_writer_elf] } {
+    set flash_writer_elf undefined
+}
+if { ! [info exists boot_bin_file] } {
+    set boot_bin_file undefined
+}
 
+####################################################################################################
+#
+# Global variables for communicating with the flash writer executables. init_flash_writer will set
+# these values.
+#
+# Writing a function ID to `tcl_function_call_variable` will call the given function. The parameters
+# can be passed via the `tcl_parameter_variable`. For more details see `_c_do` procedure.
+#
+# The `bin_slice_buffer` is the buffer to pass a chunk of the boot image to Zynq.
+# `bin_slice_buffer_size` stores the size of this buffer.
+#
+####################################################################################################
 set tcl_function_call_variable -1
 set tcl_parameter_variable -1
 set bin_slice_buffer -1
 set bin_slice_buffer_size -1
 
-proc init { } {
+
+####################################################################################################
+#
+# init_flash_writer Initialize the flash writer to be prepare any flash operation.
+#   1. Initialize Zynq's PS (processing system) (run ps7_init) 
+#   2. Donwloads the flash_writer executable to Zynq's ARM.
+#   3. Set variables to communicate with the flash_writer executable.
+#
+# @param    flash_writer_elf The path of the flash_writer_elf executable. init_flash_writer saves
+#           the path, so it don't need to specify at the second run.
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
+proc init_flash_writer { {flash_writer_elf default} } {
+
+    #
+    # Getting/saving the flash_writer_elf
+    #
+    if { $flash_writer_elf == "default" } {
+        set flash_writer_elf $::flash_writer_elf
+    }
+    if { $flash_writer_elf == "undefined" } {
+        puts "ERROR: Flash writer executable must be given at the first time."
+        return
+    }
+    if { ! [file exists $flash_writer_elf ] } {
+        puts "ERROR: Flash writer executable $flash_writer_elf does not exists."
+        return
+    }
+    set ::flash_writer_elf  [file normalize $flash_writer_elf]
+    
+    #
+    # Check and stop the processor, then run ps7_init, finally run the flash_writer executable.
+    #
     if { ! [string match Stopped* [state]] } {
         puts "stop"
         stop
@@ -34,69 +97,269 @@ proc init { } {
     dow $::flash_writer_elf
     con
     
+    #
+    # Setting global variables.
+    #
     puts "Getting communication variable (tcl_function_call)"
     set global_dict [print -dict &tcl_function_call]
     set ::tcl_function_call_variable [dict get $global_dict &tcl_function_call]
     set ::tcl_parameter_variable [expr $::tcl_function_call_variable + 4]
     
     puts "Setting slice buffer (bin_slice_buffer)"
-    set ::bin_slice_buffer      [c_do $::TCL_FUNCTION_ID_GET_SLICE_BUFFER]
-    set ::bin_slice_buffer_size [c_do $::TCL_FUNCTION_ID_GET_SLICE_BUFFER_SIZE]
+    set ::bin_slice_buffer      [_c_do $::TCL_FUNCTION_ID_GET_SLICE_BUFFER]
+    set ::bin_slice_buffer_size [_c_do $::TCL_FUNCTION_ID_GET_SLICE_BUFFER_SIZE]
+    
+    return
 }
 
-proc c_do { func_id {parameters {} } } {
 
-    if { $::tcl_function_call_variable <0 } {
-        puts "ERROR: Global variables has not been initialized. Please run the init function."
-        return
+####################################################################################################
+#
+# _c_do The lowest-level communication procedure. _c_do will call the `func_id` C function, with
+# `parameters` parameters. Do not use this function directly. Use higher level function instead.
+#
+# @param    func_id The ID of the C function to be call. See TCL_FUNCTION_ID* gloval variables.
+# @param    parameters A list of the parameters to be passed to the C function.
+#
+# @return   The return value of the original C function.
+#
+# @note     The return value will be the previous function in case of void function.
+#
+####################################################################################################
+proc _c_do { func_id {parameters {} } } {
+
+    #
+    # Check if init has been run earlier.
+    #
+    if { $::tcl_function_call_variable < 0 } {
+        puts "ERROR: Global variables has not been initialized. Please run the init_flash_writer function."
+        return -1
     }
 
+    #
+    # Wait for the previous function run.
+    #
     while {[mrd -value $::tcl_function_call_variable] != $::TCL_FUNCTION_ID_RETURN} {
     }
 
-    # Fill parameters
+    #
+    # Fill parameters and call the function
+    #
     mwr $::tcl_parameter_variable $parameters
-    # Call the function
     mwr $::tcl_function_call_variable $func_id
     
+    #
     # wait for function complete
-    
+    #
     while {[mrd -value $::tcl_function_call_variable] != $::TCL_FUNCTION_ID_RETURN} {
     }
     
     return [mrd -value $::tcl_parameter_variable]
-    
 }
 
+
+####################################################################################################
+#
+# hello simple debug function, will print a message on the standard output.
+#
+# @param    hello_param The number to be written after the hello string.
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
 proc hello { hello_param } {
-    c_do $::TCL_FUNCTION_ID_PRINT_HELLO $hello_param
+    _c_do $::TCL_FUNCTION_ID_PRINT_HELLO $hello_param
+    
+    return
 }
 
-proc flash_erase { address length } {
-    c_do $::TCL_FUNCTION_ID_FLASH_ERASE "$address $length"
+
+####################################################################################################
+#
+# print_flash_writer_version reads and prints out to TCL console the flash_write executable's
+# version.
+#
+# @param    None.
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
+proc print_flash_writer_version { } {
+    set version [_c_do $::TCL_FUNCTION_ID_GET_VERSION]
+    puts $version
+    
+    return
 }
 
-proc flash_write { address length ptr } {
-    c_do $::TCL_FUNCTION_ID_FLASH_WRITE "$address $length $ptr"
-}
 
+####################################################################################################
+#
+# flash_init after running the init_flash_writer the flash (more precisely the structures of the
+# flash) can be initialized. It reads the type, size, page size, etc. parameters from the flash.
+#
+# @param    None.
+#
+# @return   None.
+#
+# @note     TODO more trace/debug information should be added here.
+#
+####################################################################################################
 proc flash_init { } {
-    set ret [c_do $::TCL_FUNCTION_ID_FLASH_INIT]
+    set ret [_c_do $::TCL_FUNCTION_ID_FLASH_INIT]
     if { $ret != 0 } {
         puts "ERROR flash initializationhas failed."
     }
+    
+    return
 }
 
+
+####################################################################################################
+#
+# get_flash_info. After flash_init the flash properties aka. information can be read by the
+# get_flash_info procedure. This will print the flash information to TCL's console.
+#
+# @param    None.
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
+proc get_flash_info { } {
+    set flash_info_ptr [_c_do $::TCL_FUNCTION_ID_GET_FLASH_INFO]
+    set flash_info [mrd -value $::tcl_parameter_variable 9]
+    
+    puts [format "SectSize          0x%x" [lindex $flash_info 0]]
+    puts [format "NumSect           0x%x" [lindex $flash_info 1]]
+    puts [format "PageSize          0x%x" [lindex $flash_info 2]]
+    puts [format "NumPage           0x%x" [lindex $flash_info 3]]
+    puts [format "FlashDeviceSize   0x%x" [lindex $flash_info 4]]
+    puts [format "ManufacturerID    0x%x" [lindex $flash_info 5]]
+    puts [format "DeviceIDMemSize   0x%x" [lindex $flash_info 6]]
+    puts [format "SectMask          0x%x" [lindex $flash_info 7]]
+    puts [format "NumDie            0x%x" [lindex $flash_info 8]]
+    
+    return
+}
+
+
+####################################################################################################
+#
+# flash_erase Erase a block from given address to given length. Must be called before writing the
+# flash.
+#
+# @param    address is the start address of the block to be erase in bytes.
+# @param    length is the length of the block to be erase in bytes.
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
+proc flash_erase { address length } {
+    _c_do $::TCL_FUNCTION_ID_FLASH_ERASE "$address $length"
+    
+    return
+}
+
+
+####################################################################################################
+#
+# _flash_write_slice_ptr writes a slice of data to flash. Do not use this function directly. Use
+# flash_write_file instead.
+#
+# @param    address is the start address in the flash device in bytes.
+# @param    length is the length of the block to be write in bytes.
+# @param    ptr is the address of the block in the executable memory-space. Usually equals the
+#           bin_slice_buffer
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
+proc _flash_write_slice_ptr { address length ptr } {
+    _c_do $::TCL_FUNCTION_ID_FLASH_WRITE "$address $length $ptr"
+    
+    return
+}
+
+
+####################################################################################################
+#
+# _flash_write_slice_ptr writes a slice of data to flash. Do not use this function directly. Use
+# flash_write_file instead.
+#
+# @param    address is the start address in the flash device in bytes.
+# @param    length is the length of the block to be write in bytes.
+# @param    ptr is the address of the block in the executable memory-space. Usually equals the
+#           bin_slice_buffer
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
+proc flash_write_slice { address length data_slice {ptr "default" } } {
+    if { $ptr == "default" } {
+        set ptr $::bin_slice_buffer
+    }
+    
+    #
+    # Copy data-slice from TCL to Zynq on-chip-memory
+    #
+    mwr -size b [expr $::bin_slice_buffer +4 ] $data_slice
+    
+    #
+    # Ask to write data-slice from Zynq on-chip-memory to flash
+    #
+    _flash_write_slice_ptr $address $length $ptr
+    
+    return
+}
+
+
+####################################################################################################
+#
+# flash_dump_zynq prints a slice of data to Zynq's standard out.
+#
+# @param    address is the start address in the flash device in bytes to dump.
+# @param    length is the length of the block to dump in bytes.
+#
+# @return   None.
+#
+# @note     Use flash_dump instead, which prints data locally
+#
+####################################################################################################
+proc flash_dump_zynq { address length } {
+    _c_do $::TCL_FUNCTION_ID_FLASH_DUMP "$address $length"
+    
+    return
+}
+
+
+####################################################################################################
+#
+# flash_dump prints a slice of data to TCL console in a hexdump-like format.
+#
+# @param    address is the start address in the flash device in bytes to dump.
+# @param    length is the length of the block to dump in bytes.
+#
+# @return   None.
+#
+# @note     None.
+#
+####################################################################################################
 proc flash_dump { address length } {
-    c_do $::TCL_FUNCTION_ID_FLASH_DUMP "$address $length"
-}
-
-proc flash_read { address length } {
-     set read_data_prt [c_do $::TCL_FUNCTION_ID_FLASH_READ "$address $length"]
+     set read_data_prt [_c_do $::TCL_FUNCTION_ID_FLASH_READ "$address $length"]
      set read_data [mrd -value $read_data_prt $length]
     
     # Put the hex and Latin-1 data to the channel
-   
     set hex ""
     set len_32 [expr $length/4]
     for {set i 0} {$i<$len_32} {incr i} {
@@ -107,44 +370,68 @@ proc flash_read { address length } {
             set hex ""
         }
     }
-}
-
-proc print_version { } {
-    set version [c_do $::TCL_FUNCTION_ID_GET_VERSION]
-    puts $version
-}
-
-proc get_flash_info { } {
-    set flash_info_ptr [c_do $::TCL_FUNCTION_ID_GET_FLASH_INFO]
-    set flash_info [mrd -value $::tcl_parameter_variable 9]
     
-	puts [format "SectSize          0x%x" [lindex $flash_info 0]]
-	puts [format "NumSect           0x%x" [lindex $flash_info 1]]
-	puts [format "PageSize          0x%x" [lindex $flash_info 2]]
-	puts [format "NumPage           0x%x" [lindex $flash_info 3]]
-	puts [format "FlashDeviceSize   0x%x" [lindex $flash_info 4]]
-	puts [format "ManufacturerID    0x%x" [lindex $flash_info 5]]
-	puts [format "DeviceIDMemSize   0x%x" [lindex $flash_info 6]]
-	puts [format "SectMask          0x%x" [lindex $flash_info 7]]
-	puts [format "NumDie            0x%x" [lindex $flash_info 8]]
+    return
 }
 
-proc flash_write2 { filename } {
 
-    set hex_data [file_to_hex $filename]
-    set image_size [file size $filename]
+####################################################################################################
+#
+# flash_write_file writes a complete file to flash.
+#
+# @param    boot_bin_file the name of the file to be written.
+#
+# @return   None.
+#
+# @note     Erase befor write.
+#
+####################################################################################################
+proc flash_write_file { {boot_bin_file "default"} {verbose 1} } {
+
+    #
+    # Getting/saving the boot_bin_file
+    #
+    if { $boot_bin_file == "default" } {
+        set boot_bin_file $::boot_bin_file
+    }
+    if { $boot_bin_file == "undefined" } {
+        puts "ERROR: Boot image file must be given at the first time."
+        return
+    }
+    if { ! [file exists $boot_bin_file ] } {
+        puts "ERROR: Boot image file $boot_bin_file does not exists."
+        return
+    }
+    set ::boot_bin_file  [file normalize $boot_bin_file]
     
+    #
+    # Parse the file
+    #
+    if { $verbose } {
+        puts -nonewline "Parse file..."
+        flush stdout
+    }
+    set hex_data [file_to_hex $boot_bin_file]
+    set image_size [file size $boot_bin_file]
+    if { $verbose } {
+        puts "        OK."
+    }
+    
+    #
+    # Write boot image slice by slice
+    #
     for {set i 0} {$i<$image_size} {incr i $::bin_slice_buffer_size} {
     
-        puts "$i"
+        if { $verbose } {
+            puts [format "0x%04x / 0x%x kB   %%%d" [expr $i / 1024] [expr $image_size / 1024] [expr 100 * $i / $image_size]]
+        }
     
         set hex_slice [lrange $hex_data $i [expr $i+$::bin_slice_buffer_size-1]]
         
-        mwr -size b [expr $::bin_slice_buffer +4 ] $hex_slice
-        
-        flash_write $i $::bin_slice_buffer_size $::bin_slice_buffer
+        flash_write_slice $i $::bin_slice_buffer_size $hex_slice
     }
     
+    return
 }
 
 
@@ -165,6 +452,8 @@ proc file_to_hex { filename  } {
     regsub -all -- {..} $hex {0x& } hex
     
     return $hex
+    
+    return
 }
 
 
@@ -207,5 +496,13 @@ proc hexdump { filename { channel stdout } } {
      }
      # When we're done, close the file.
      catch { ::close $fid }
+     
      return
- }
+}
+
+#
+# Check existance of ps7_init procedure
+#
+if { [info procs ps7_init] != "ps7_init" } {
+    puts "Warning! ps7_init procedure not found! Please source your ps7_init.tcl, which is placed under the hardware platorm."
+}
